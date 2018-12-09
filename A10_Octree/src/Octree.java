a1a1c8a021f384aaae0ef98ee0fbe67752c66c8a
Grant Zheng <Grant Zheng@DESKTOP-D6N497R>
2018-12-08 23:50:58 -0500

    Started the project

---
+++ A10_Octree/src/Octree.java
@@ -0,0 +1,135 @@
+import java.util.ArrayList;
+
+
+public class Octree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static ArrayList<double[]> traverseTree(Sphere sphere, Cube cube, int level, ArrayList<double[]> cubes){
+		if (level >= 5) return cubes;
+	}
+	
+	/**
+	 * This method calculates whether a cube is totally in a sphere/
+	 * If the corner of the cube overlap with the sphere surface, it
+	 * is still considered as all in.
+	 * 
+	 * @param cube a Cube object
+	 * @param sphere a Sphere object
+	 * @return a boolean, true if the cube is all in, false otherwise
+	 */
+	public static boolean allIn(Cube cube, Sphere sphere) {
+		ArrayList<double[]> cubeCorners = cube.calculateCorners();
+		
+		for (int i = 0; i < cubeCorners.size(); i ++) {
+			double currDist = calculateDist(cubeCorners.get(i), sphere.center);
+			if (currDist > sphere.radius) 
+				return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * This method calculates the Euclidean distance
+	 * between two points 
+	 * 
+	 * @param c1 point 1, a double array of size 3
+	 * @param c2 point 2, a double array of size 3
+	 * @return a double of the distances in absolute value
+	 */
+	public static double calculateDist(double[] c1, double[] c2) {
+		double toReturn = 0;
+		for (int i = 0; i < 3; i ++) {
+			toReturn = (c1[i] - c2[i]) * (c1[i] - c2[i]);
+		}
+		
+		toReturn = Math.sqrt(toReturn);
+		return toReturn;
+	}
+	
+	
+	/**
+	 * This is the inner class for Octree,
+	 * Cube class contains information about a cube
+	 * @author Grant Zheng
+	 *
+	 */
+	class Cube{
+		double x, y, z;
+		double width, height, depth;
+		
+		/**
+		 * The constructor takes two double list for information
+		 * that can describe the cube
+		 * 
+		 * @param center a double list with a length 3 that indicates the
+		 * coordinate of the center location of the cube
+		 * 
+		 * @param xyzLimits a double list with a length 3 that indicates 
+		 * the width, height and depth. They corresponds to the x, y and z
+		 * coordinates respectively
+		 */
+		public Cube(double center[], double[] xyzLimits) {
+			this.x = center[0];
+			this.y = center[1];
+			this.z = center[2];
+			this.width = xyzLimits[0];
+			this.height = xyzLimits[1];
+			this.depth = xyzLimits[2];
+		}
+		
+		/**
+		 * This method calculates and return all 8 corners of the cube
+		 * 
+		 * The corners are in counter-clockwise direction from the 
+		 * upper left corner. Front then back
+		 * 
+		 * @return an ArrayList of double[] that indicates the corner locations
+		 */
+		
+		public ArrayList<double[]> calculateCorners(){
+			ArrayList<double[]> toReturn = new ArrayList<>();
+			double halfWidth = this.width / 2;
+			double halfHeight = this.height / 2;
+			double halfDepth = this.depth / 2;
+			
+			double[] c0 = {this.x - halfWidth, this.y - halfHeight, this.z - halfDepth};
+			double[] c1 = {this.x - halfWidth, this.y + halfHeight, this.z - halfDepth};
+			double[] c2 = {this.x + halfWidth, this.y + halfHeight, this.z - halfDepth};
+			double[] c3 = {this.x + halfWidth, this.y - halfHeight, this.z - halfDepth};
+			double[] c4 = {this.x - halfWidth, this.y - halfHeight, this.z + halfDepth};
+			double[] c5 = {this.x - halfWidth, this.y + halfHeight, this.z + halfDepth};
+			double[] c6 = {this.x + halfWidth, this.y + halfHeight, this.z + halfDepth};
+			double[] c7 = {this.x + halfWidth, this.y - halfHeight, this.z + halfDepth};
+			
+			toReturn.add(c0);
+			toReturn.add(c1);
+			toReturn.add(c2);
+			toReturn.add(c3);
+			toReturn.add(c4);
+			toReturn.add(c5);
+			toReturn.add(c6);
+			toReturn.add(c7);
+			
+			return toReturn;
+		}
+	}
+	
+	/**
+	 * 
+	 * @author Grant Zheng
+	 *
+	 */
+	class Sphere{
+		double center[];
+		double radius;
+		public Sphere(double center[], double radius ) {
+			this.center = center;
+			this.radius = radius;
+		}
+	}
+
+}
---
+++ A10_Octree/src/voxel.java
@@ -0,0 +1,166 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class voxel {
+	
+	/*
+	 *                 24786  GEOMETRIC MODELING
+	 *                 Carnegie Mellon University
+	 *                          VOXEL
+	 */
+	
+	/*
+	 * constants
+	 */
+	
+	static double BOXPOSX = -10.0;       /* x position of voxel box          */
+	static double BOXPOSY = -10.0;       /* y position of voxel box          */
+	static double BOXPOSZ = -10.0;       /* z position of voxel box          */
+	static double BOXSIZE = 20.0;        /* size of the voxel box            */
+	static int    NDIV    = 16;          /* number of division in x,y,z axes */
+	
+	static double WIDTH   = 18.0;        /* width of the cube (x direction)  */
+	static double HEIGHT  = 10.0;        /* height of the cube (y direction) */
+	static double DEPTH   = 12.0;        /* depth of the cube (z direction)  */
+	
+	static double CUBX1   = -9.0;        /* min-x face of the cube           */
+	static double CUBX2   =  9.0;        /* max-x face of the cube           */
+	static double CUBY1   = -5.0;        /* min-y face of the cube           */
+	static double CUBY2   =  5.0;        /* max-y face of the cube           */
+	static double CUBZ1   = -6.0;        /* min-z face of the cube           */
+	static double CUBZ2   =  6.0;        /* max-z face of the cube           */
+	
+	static double RADIUS  =  8.0;        /* radius of the sphere             */
+	static double RADIUS2 = 64.0;        /* radius of the sphere **2         */
+	
+	public static void main(String[] args) throws Exception{
+	  int       i, j, k;                   /* voxel index in x,y,z          */
+	  double    x0, y0, z0;                /* sphere center position        */
+	  double    x, y, z;                   /* current cell position         */
+	  double    cellsize = BOXSIZE/NDIV;   /* current cell size             */
+	  
+	  
+	  /*
+	   * Create a buffered reader to read the user input from the console
+	   * User will enter three doubles to specify the sphere's center
+	   */
+	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	  
+	  System.out.println("Input coordinates (e.g. 2 0 0):");
+	  StringTokenizer st = new StringTokenizer(br.readLine());
+	  
+	  x0 = Double.parseDouble(st.nextToken());
+	  y0 = Double.parseDouble(st.nextToken());
+	  z0 = Double.parseDouble(st.nextToken());
+	
+	  /* open the target VRML file, "voxel.wrl" */
+	  PrintStream output = new PrintStream(new FileOutputStream(new File("voxel.wrl")));
+	
+	  output.print("#VRML V2.0 utf8\n\n");
+	  /* draw the cube and the sphere */
+	  output.print("Background { skyColor 1 1 1 }\n");
+	  output.print("NavigationInfo { type \"EXAMINE\"}\n");
+	
+	  output.print("Shape {\n");
+	  output.print("\tappearance Appearance { material Material { diffuseColor 0 0 1 }}\n");
+	  output.print("\tgeometry Box { size " + WIDTH + " " + HEIGHT + " " + DEPTH + "}\n");
+	  output.print("}\n");
+	
+	  output.print("Transform {\n");
+	  output.print("\ttranslation " + x0 + " " + y0 + " " + z0 + "\n");
+	  output.print("\tchildren Shape {\n");
+	  output.print("\t\tgeometry Sphere { radius " + RADIUS + "}\n");
+	  output.print("\t\tappearance Appearance { material Material { diffuseColor 1 0 0 }}\n");
+	  output.print("\t}\n");
+	  output.print("}\n");
+	
+	  output.print("Transform {\n");
+	  output.print("\ttranslation 0.0 " + 1.5*BOXSIZE + "0.0\n");
+	  output.print("\tchildren [\n");
+	
+	  
+	  /*
+	   *  visit each voxel and draw a cube if the voxel center is inside
+	   *  the solid defined as: Solid = Cube -* Sphere.
+	   */
+	  for(k=0; k<NDIV; k++){
+	    for(j=0; j<NDIV; j++){
+	      for(i=0; i<NDIV; i++){
+	        x = BOXPOSX + i*cellsize;
+	        y = BOXPOSY + j*cellsize;
+	        z = BOXPOSZ + k*cellsize;
+	        if ( in_cube(x,y,z) == 1 && in_sphere(x,y,z,x0,y0,z0) == 0 ) {
+			  output.print("\tTransform {\n");
+			  output.print("\t\ttranslation " + x + " " + y + " " + z + "\n");
+			  output.print("\t\tchildren Shape {\n");
+			  output.print("\t\t\tappearance Appearance { material Material { diffuseColor 0 0 1 }}\n");
+			  output.print("\t\t\tgeometry Box {size " + 0.9*cellsize + " " + 0.9*cellsize + " " + 0.9*cellsize + "}\n");
+	          output.print("\t\t}\n");
+	          output.print("\t},\n");
+	        }
+	      }
+	    }
+	  }
+	
+	  output.print("\t]\n");  // close children
+	  output.print("}\n");      // close Transform
+	
+	
+	  /* close the target VRML file "output.wrl" */
+	  output.close();
+	}
+	
+	
+	/*
+	 * Check if the point is inside the cube. Return 1 if inside, 0 if outside.
+	 */
+	public static int in_cube(double x, double y, double z){
+	  if (x < CUBX1 || CUBX2 < x || y < CUBY1 || CUBY2 < y
+	                || z < CUBZ1 || CUBZ2 < z)
+	     return 0;
+	  else return 1;
+	}
+	
+	/*
+	 * Check if the point is inside the sphere. Return 1 if inside, 0 if outside.
+	 */
+	public static int in_sphere(double x, double y, double z, double x0, double y0, double z0){
+	  if ( ((x-x0)*(x-x0) + (y-y0)*(y-y0) + (z-z0)*(z-z0) ) < RADIUS2 )
+	     return 1;
+	  else return 0;
+	}
+	
+	public static ArrayList<double[]> traverseTree(Sphere sphere, Cube cube, int level, ArrayList<double[]> cubes){
+		if (level >= 5) return cubes;
+	}
+	
+	class Cube{
+		double x, y, z;
+		double width, height, depth;
+		public Cube(double center[], double[] xyzLimits) {
+			this.x = center[0];
+			this.y = center[1];
+			this.z = center[2];
+			this.width = xyzLimits[0];
+			this.height = xyzLimits[1];
+			this.depth = xyzLimits[2];
+		}
+	}
+	
+	class Sphere{
+		double center[];
+		double radius;
+		public Sphere(double center[], double radius ) {
+			this.center = center;
+			this.radius = radius;
+		}
+	}
+}
+	
+	
+	
+	
+	
+	
+	
